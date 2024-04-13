3c65c63d62e27282cc9820f800a7214aa8ca4c98
DavidTchintcharauli <chincharaulidavid@gmail.com>
2024-04-13 13:54:55 +0400

    change App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect} from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost/backend/api.php')
+      .then(response => response.json())
+      .then(data => setItems(data))
+      .catch(error => console.error('Error fetching data:', error))
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Item from PHP Backend:</h1>
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+    </div>
   )
 }
 
